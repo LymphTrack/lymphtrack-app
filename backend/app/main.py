98342db8f5430a7dbee2c6057f55429e864da2ae
Pimprenelle <pimprenelle.simon@reseau.eseo.fr>
2025-11-12 10:58:53 +0100

    feat: Update CORS settings and upgrade expo dependencies to latest versions

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,6 +17,7 @@ app.add_middleware(
         "http://130.238.147.150:8000",
         "https://lymphtrack.loca.lt",
         "https://lymphtrack-app.vercel.app",
+        "http://localhost:8000"
         "*",    
     ],
     allow_credentials=True,
